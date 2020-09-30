5f3e908815e399d0bb3d6345ad859b126a048c82
ktjd1262 <k9326239@gmail.com>
2020-09-30 16:47:05 +0900

    update eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,9 +1,19 @@
 module.exports = {
   root: true,
-  extends: ['airbnb', '@react-native-community'],
+  extends: ['airbnb', '@react-native-community', 'plugin:import/typescript'],
   parser: '@typescript-eslint/parser',
   plugins: ['@typescript-eslint'],
   rules: {
     'react/jsx-filename-extension': [1, {extensions: ['.tsx']}],
+    'import/extensions': [
+      'error',
+      'ignorePackages',
+      {
+        js: 'never',
+        jsx: 'never',
+        ts: 'never',
+        tsx: 'never',
+      },
+    ],
   },
 };
